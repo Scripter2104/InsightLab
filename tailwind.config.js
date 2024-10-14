e9087e29860b27e756349ca739767a87f4f86827
Scripter2104 <yashvsolanki81@gmail.com>
2024-10-14 21:21:30 +0530

    Created Project and added one app loginApp

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './InsightLab/loginApp/templates/**/*.html',  // Path to your HTML templates
+    './InsightLab/static/**/*.css',                // Path to your CSS files
+    './InsightLab/static/**/*.js',                 // Path to your JavaScript files if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

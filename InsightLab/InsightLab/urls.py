e9c7a4e12b5837a69bfe0febec747f2f3319ed3c
Scripter2104 <yashvsolanki81@gmail.com>
2024-10-15 15:28:06 +0530

    Added functionality of login and signup to home page

--- InsightLab/InsightLab/urls.py
+++ InsightLab/InsightLab/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from loginApp.views import login_view
 from loginApp.views import signup_view
+from homeApp.views import home_view
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
-    path('',login_view,name='home'),
-    path('signup/',signup_view,name='signup')
+    # path('admin/', admin.site.urls),
+    path('', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('home/', home_view, name='home'),
 ]
--- InsightLab/loginApp/views.py
+++ InsightLab/loginApp/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 # Create your views here.
-def login_view(request, *args, **kwargs):
-    return render(request, 'login_page.html', {})
-
-
 def signup_view(request, *args, **kwargs):
-    return render(request, 'signup_page.html', {})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+
+        form = UserCreationForm()
+    return render(request, 'signup_page.html', {'form': form})
+
+
+def login_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return render(request, 'login_page.html', {'error': 'Invalid username or password.'})
+
+    return render(request, 'login_page.html', {})

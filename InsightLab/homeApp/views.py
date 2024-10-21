776a0c9d38f88a3a6783aa00188e6adc79b9a8ed
Scripter2104 <yashvsolanki81@gmail.com>
2024-10-21 14:29:15 +0530

    added functionalities to store the selected choices of a user for creating the test

--- InsightLab/homeApp/views.py
+++ InsightLab/homeApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Test
+from django.urls import reverse
 
 # Create your views here.
 
@@ -13,3 +14,15 @@ def newtest_view(request, *args, **kwargs):
 
 def test_start_page_config(request, *args, **kwargs):
     return render(request, 'test_start_page.html', {})
+
+
+def start_test(request, *args, **kwargs):
+    return render(request, 'start_test.html', {})
+def activate_test(request, test_id):
+    test = Test.objects.get(id=test_id)
+
+    if not test.name:
+        return render(request,'time_setting.html',{"message":"All field are nor filled!"})
+
+    test.is_active=True
+    test.link=request.build_absolute_uri(reverse('test_start_pa'))

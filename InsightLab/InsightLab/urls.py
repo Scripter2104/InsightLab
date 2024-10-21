fd4a18ee136396627345d46d1a64a9948373234c
Scripter2104 <yashvsolanki81@gmail.com>
2024-10-21 20:52:23 +0530

    Added everything in templated except time settings

--- InsightLab/InsightLab/urls.py
+++ InsightLab/InsightLab/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from loginApp.views import login_view
 from loginApp.views import signup_view
-from homeApp.views import home_view, newtest_view, test_start_page_config
+from homeApp.views import home_view, newtest_view, test_start_page_config, question_manager_view, grading_summary_view
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -26,6 +26,8 @@ urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('home/', home_view, name='home'),
     path('newtest/', newtest_view, name='newtest'),
-    path('newtest/test start page/', test_start_page_config, name='test_start_page')
+    path('newtest/test start page/', test_start_page_config, name='test_start_page'),
+    path('newtest/question manager/', question_manager_view, name='question_manager'),
+    path('newtest/grading and summary', grading_summary_view, name='grading_and_summary')
 
 ]

eb932c625000e2c0305b3c30dfda5cc038822012
Scripter2104 <yashvsolanki81@gmail.com>
2024-10-23 16:51:16 +0530

    Except for question manager every page retains memory

--- InsightLab/InsightLab/urls.py
+++ InsightLab/InsightLab/urls.py
@@ -18,16 +18,18 @@ from django.contrib import admin
 from django.urls import path
 from loginApp.views import login_view
 from loginApp.views import signup_view
-from homeApp.views import home_view, newtest_view, test_start_page_config, question_manager_view, grading_summary_view
+from homeApp.views import home_view, newtest_view, test_start_page_config, question_manager_view, grading_summary_view, \
+    time_setting_view
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('home/', home_view, name='home'),
-    path('newtest/', newtest_view, name='newtest'),
+    path('newtest/basic setting', newtest_view, name='newtest'),
     path('newtest/test start page/', test_start_page_config, name='test_start_page'),
     path('newtest/question manager/', question_manager_view, name='question_manager'),
-    path('newtest/grading and summary', grading_summary_view, name='grading_and_summary')
+    path('newtest/grading and summary', grading_summary_view, name='grading_and_summary'),
+    path('newtest/time setting', time_setting_view, name='time_setting'),
 
 ]
